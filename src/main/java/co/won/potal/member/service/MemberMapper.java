78df6ea702111a25b06e5f40ed3b9224366e6876
ghdnjs18 <ghdnjs18@naver.com>
2021-12-15 11:28:45 +0900

    memberMapper 설정

---
+++ src/main/java/co/won/potal/member/service/MemberMapper.java
@@ -0,0 +1,12 @@
+package co.won.potal.member.service;
+
+import java.util.List;
+
+public interface MemberMapper {
+	List<MemberVO> memberSelectList();
+	MemberVO memberSelerct(MemberVO vo); // 한명 정보 조회 / 로그인 채크
+	int memberInsert(MemberVO vo);
+	int memberUpdate(MemberVO vo);
+	int memberDelete(MemberVO vo);
+	boolean memberIdCheck(String id); // 아이디 중복 체크용
+}
