8aee24b68d3de83e22c6b40a33b8d8ceaf58d5c8
ghdnjs18 <ghdnjs18@naver.com>
2021-12-15 11:34:20 +0900

    파일 업데이트

---
+++ src/main/java/co/won/potal/member/serviceImpl/MemberServiceImpl.java
@@ -0,0 +1,43 @@
+package co.won.potal.member.serviceImpl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import co.won.potal.member.service.MemberService;
+import co.won.potal.member.service.MemberVO;
+
+@Repository
+public class MemberServiceImpl implements MemberService {
+
+	public List memberSelectList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public MemberVO memberSelerct(MemberVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int memberInsert(MemberVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int memberUpdate(MemberVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int memberDelete(MemberVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean memberIdCheck(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
