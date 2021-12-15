7a2f0358052b5e3218277d6f2cd510f9d2a6caea
ghdnjs18 <ghdnjs18@naver.com>
2021-12-15 11:41:17 +0900

    memberServiceImlp 설정

--- src/main/java/co/won/potal/member/serviceImpl/MemberServiceImpl.java
+++ src/main/java/co/won/potal/member/serviceImpl/MemberServiceImpl.java
@@ -2,42 +2,46 @@ package co.won.potal.member.serviceImpl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import co.won.potal.member.service.MemberMapper;
 import co.won.potal.member.service.MemberService;
 import co.won.potal.member.service.MemberVO;
 
-@Repository
+@Repository("memberDao")
 public class MemberServiceImpl implements MemberService {
-
-	public List memberSelectList() {
-		// TODO Auto-generated method stub
-		return null;
+	@Autowired
+	MemberMapper map;
+		
+	@Override
+	public List<MemberVO> memberSelectList() {
+		return map.memberSelectList();
 	}
 
+	@Override
 	public MemberVO memberSelerct(MemberVO vo) {
-		// TODO Auto-generated method stub
-		return null;
+		return map.memberSelerct(vo);
 	}
 
+	@Override
 	public int memberInsert(MemberVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		return map.memberInsert(vo);
 	}
 
+	@Override
 	public int memberUpdate(MemberVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		return map.memberUpdate(vo);
 	}
 
+	@Override
 	public int memberDelete(MemberVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		return map.memberDelete(vo);
 	}
 
+	@Override
 	public boolean memberIdCheck(String id) {
-		// TODO Auto-generated method stub
-		return false;
+		return map.memberIdCheck(id);
 	}
 
 }
